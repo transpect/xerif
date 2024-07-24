8c55e1a3a6fb3d5df6742e331a63eea24afcb7f1
github svnbridge <fraenze.gramsch@le-tex.de>
2024-07-24 10:08:42 +0200

    restore merlin host

--- latex-oops/paths.sh
+++ latex-oops/paths.sh
@@ -15,6 +15,8 @@ elif [ "$THISHOST" = "transcript-prod" ]; then
     XINDY=/usr/local/texlive/2019early/bin/x86_64-linux/xindy
 elif [ "$THISHOST" = "transpect" ]; then
     XINDY=/usr/local/texlive/2019/bin/x86_64-linux/xindy
+elif [ "$THISHOST" = "merlin" ]; then
+    XINDY=/usr/local/texlive/2019/bin/x86_64-linux/xindy
 else
     XINDY=$(which xindy)
 fi
