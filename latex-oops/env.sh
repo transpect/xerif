4e9eccd80ab436ab997be158b82a3c18aaee299b
github svnbridge <fraenze.gramsch@le-tex.de>
2024-09-10 11:44:13 +0200

    delete obsolete latex and latex-oops (moved to https://subversion.le-tex.de/common/github-mirror/transpect/xerif-latex/trunk/ -  don't forget to add to your project externals!)

--- latex-oops/env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export LUAINPUTS=.:ltpdfa:
-export CLUAINPUTS=.:ltpdfa//:
-export TEXINPUTS=.:ltpdfa:
-export T1FONTS=.:ltpdfa:
-export TFMFONTS=.:ltpdfa:
-export ENCFONTS=.:ltpdfa:
-export DVIPSHEADERS=.:suppl:
